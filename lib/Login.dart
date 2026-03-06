1739d2ebfe4b4df8e69ec85c2af3e573fd1958be
saadan <saadan@ca-eim.com>
2026-03-06 09:33:18 +0400

    "commit from new Mac"

--- lib/Login.dart
+++ lib/Login.dart
@@ -1215,7 +1215,7 @@ class _LoginPageState extends State<Login>  with TickerProviderStateMixin {
                 </div>''';
     try {
 
-      // await send(message, smtpServer); // ✅ DO NOT assign it to a variable
+      await send(message, smtpServer); // ✅ DO NOT assign it to a variable
 
       /*_scaffoldMessengerKey.currentState?.showSnackBar(
         SnackBar(

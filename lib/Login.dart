9acf339d236805ce2569de268b6996e97f70658b
saadan <saadan@ca-eim.com>
2026-05-01 16:41:30 +0400

    "commit from new Mac"

--- lib/Login.dart
+++ lib/Login.dart
@@ -1225,7 +1225,6 @@ class _LoginPageState extends State<Login>  with TickerProviderStateMixin {
         ),
       );*/
 
-
       /*print('Message sent: ${sendReport.toString()}');*/
     }
     catch (e)
